2b6aaf4ecac0a56fc7b3e788d777aff6e1381554
Hrothgor <jul.minguet@gmail.com>
2021-12-06 15:03:17 +0100

    first commit

--- build/CMakeFiles/obs-easyStream_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/obs-easyStream_autogen"
-  "obs-easyStream_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/obs-easyStream_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
