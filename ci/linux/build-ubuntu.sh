6df601a955b16f37d866ecfff44c8219c64f1a99
Maximilien Drules <maximilien.drules@epitech.eu>
2021-12-10 15:43:49 +0100

    Feature GitHub ci (#23)
    * Create c-cpp.yml
    
    * Feat: trying to add ubuntu dockerfile
    
    * Fix: trying to make docker work
    
    * Fix: ubuntuDocker Image works well
    
    * Fix: updating ubuntuDocker Image
    
    * Feat: editing github ci
    
    * Test: not compiling
    
    * Test: comile
    
    * fix: delete wrong file
    
    * Fix: trying to fix .yml
    
    * Fix: only on pr
    
    * Fix: on pr only
    
    * Test: macosBuild
    
    * Test: debug
    
    * o
    
    * o
    
    * Test: macos
    
    * test
    
    * test
    
    * test
    
    * test
    
    * Update build.yml
    
    * Update build.yml
    
    * Fix: trying without plugin folder
    
    * Test
    
    * Feat: documentation genration
    
    * Feat: adding dos folder
    
    * Test: trying to add documentation
    
    * Test: docs generation
    
    * Fix: workflows

--- ci/linux/build-ubuntu.sh
+++ ci/linux/build-ubuntu.sh
@@ -2,5 +2,5 @@
 set -ex
 
 sudo rm -rf build && mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_UBUNTU=yes ../plugin
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_UBUNTU=yes ..
 make -j4
