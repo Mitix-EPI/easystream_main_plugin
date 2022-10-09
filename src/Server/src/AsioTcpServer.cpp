0a1b47a4b2c24a954d9958b8fa316baaf431c043
YerimB <romain.bry@epitech.eu>
2022-10-09 18:08:21 +0200

    [server] - Create scene switch trigger endpoint

--- src/Server/src/AsioTcpServer.cpp
+++ src/Server/src/AsioTcpServer.cpp
@@ -9,10 +9,11 @@
 
 using nlohmann::json;
 
-es::server::AsioTcpServer::AsioTcpServer(const std::string &host, int port, const std::unordered_map<std::string, std::shared_ptr<obs::AutoAudioLeveler>> &_mps): _audioLeveler(_mps), _endPoint(boost::asio::ip::make_address(host), port), _acceptor(_ioContext, _endPoint)
+es::server::AsioTcpServer::AsioTcpServer(const std::string &host, int port, const std::unordered_map<std::string, std::shared_ptr<obs::AutoAudioLeveler>> &_mps) : _audioLeveler(_mps), _endPoint(boost::asio::ip::make_address(host), port), _acceptor(_ioContext, _endPoint)
 {
     _handler["getAllMics"] = &AsioTcpServer::getAllMics;
     _handler["setVolumeToMic"] = &AsioTcpServer::setVolumeToMic;
+    _handler["setSceneSwapTrigger"] = &AsioTcpServer::setSceneSwapTrigger;
 }
 
 es::server::AsioTcpServer::~AsioTcpServer()
@@ -21,20 +22,26 @@ es::server::AsioTcpServer::~AsioTcpServer()
 
 bool es::server::AsioTcpServer::start()
 {
-    try {
+    try
+    {
         waitForClientConnection();
-        _threadContext = boost::thread([this](){_ioContext.run();});
+        _threadContext = boost::thread([this]()
+                                       { _ioContext.run(); });
         blog(LOG_INFO, "[SERVER EASYSTREAM] new server started on port: %d", _endPoint.port());
-    } catch (std::exception &e) {
+    }
+    catch (std::exception &e)
+    {
         blog(LOG_INFO, "[SERVER EASYSTREAM] Exception: %s", e.what());
         return false;
     }
+
     return true;
 }
 
 void es::server::AsioTcpServer::waitForClientConnection()
 {
-    _acceptor.async_accept([this] (boost::system::error_code ec, boost::asio::ip::tcp::socket sock) {
+    _acceptor.async_accept([this](boost::system::error_code ec, boost::asio::ip::tcp::socket sock)
+                           {
         if (!ec) {
             std::cout << "[SERVER EASYSTREAM] new connection detected " << sock.remote_endpoint() << std::endl;
             _connections.push_back(boost::make_shared<AsioTcpConnection>(sock));
@@ -47,8 +54,7 @@ void es::server::AsioTcpServer::waitForClientConnection()
         } else {
             std::cout << "[SERVER EASYSTREAM] connection denied: " << ec.message() << std::endl;
         }
-        waitForClientConnection();
-    });
+        waitForClientConnection(); });
 }
 
 boost::asio::io_context &es::server::AsioTcpServer::getContext()
@@ -64,15 +70,16 @@ void es::server::AsioTcpServer::writeMessage(const std::string &msg)
 
 void es::server::AsioTcpServer::update()
 {
-    _connections.erase(std::remove_if(_connections.begin(), _connections.end(), [this](const boost::shared_ptr<AsioTcpConnection> con){
-        return (!con->isConnected());
-    }), _connections.end());
-    for (auto &con: _connections) {
+    _connections.erase(std::remove_if(_connections.begin(), _connections.end(), [this](const boost::shared_ptr<AsioTcpConnection> con)
+                                      { return (!con->isConnected()); }),
+                       _connections.end());
+    for (auto &con : _connections)
+    {
         std::vector<nlohmann::json> _requests = con->getMessage();
         if (_requests.empty())
             continue;
-        for (const auto &req: _requests)
-            (this->*_handler[req["command"]])(req, con); 
+        for (const auto &req : _requests)
+            (this->*_handler[req["command"]])(req, con);
     }
 }
 
@@ -81,7 +88,8 @@ void es::server::AsioTcpServer::getAllMics(const nlohmann::json &j, boost::share
     std::vector<json> mics = es::utils::obs::listHelper::GetMicsList();
     json toSend;
 
-    for (auto &m: mics) {
+    for (auto &m : mics)
+    {
         float tmpValue = _audioLeveler.find(m["micName"])->second->getDesiredLevel() + 60;
         m["value"] = floor((tmpValue * 100) / 60);
         m["isActive"] = _audioLeveler.find(m["micName"])->second->isActive();
@@ -99,11 +107,14 @@ void es::server::AsioTcpServer::setVolumeToMic(const nlohmann::json &j, boost::s
     float value = j["args"]["value"];
 
     auto tmp = _audioLeveler.find(j["args"]["micId"]);
-    if (tmp == _audioLeveler.end()) {
+    if (tmp == _audioLeveler.end())
+    {
         // std::string tmpJ = j["micId"];
         toSend["statusCode"] = 404;
         toSend["message"] = " not found";
-    } else {
+    }
+    else
+    {
         float level = (value * 60) / 100;
         level -= 60;
         std::cout << "level is: " << level << std::endl;
@@ -114,10 +125,45 @@ void es::server::AsioTcpServer::setVolumeToMic(const nlohmann::json &j, boost::s
     con->writeMessage(toSend.dump());
 }
 
+void es::server::AsioTcpServer::setSceneSwapTrigger(const nlohmann::json &j, boost::shared_ptr<AsioTcpConnection> &con)
+{
+    json toSend;
+    int j_trigger_type = j["args"]["triggerType"];
+    auto j_trigger_value = std::string(j["args"]["triggerValue"]);
+    auto j_target_scene = std::string(j["args"]["targetScene"]);
+
+    // Check if type in TriggerType enum values
+    if (j["args"]["triggerType"] < 0 || j["args"]["triggerType"] > 1)
+    {
+        toSend["statusCode"] = 404;
+        toSend["message"] = std::string("Trigger type not found");
+    }
+    else if (false)
+    {
+        // Check if j_trigger_value & j_target_scene are valid
+    }
+    else // Request is valid
+    {
+        auto triggerInstruction = st_instruction_t({TriggerType(j_trigger_type),
+                                                    j_trigger_value,
+                                                    j_target_scene});
+        this->_sceneSwapTriggers.push_back(triggerInstruction);
+
+        blog(
+            LOG_INFO,
+            "[SERVER EASYSTREAM] added instruction :\n--- type : %d\n--- value : %s\n--- target_scene_name : %s",
+            triggerInstruction.type,
+            triggerInstruction.value.c_str(),
+            triggerInstruction.target_scene_name.c_str());
+
+        toSend["statusCode"] = 200;
+        toSend["message"] = std::string("OK");
+    }
+    con->writeMessage(toSend.dump());
+}
 
 // 60 -> 100
 // 0
-
 
 //-40 + 60
 //
